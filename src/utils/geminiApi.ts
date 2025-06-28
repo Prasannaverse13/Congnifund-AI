01aefde9e070b83d93e5231767e40af6a91baa91
Prasannaram <116105281+Prasannaverse13@users.noreply.github.com>
2025-06-28 17:11:50 +0530

    Update geminiApi.ts

--- src/utils/geminiApi.ts
+++ src/utils/geminiApi.ts
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = 'AIzaSyBxdNfAxjzBsqS_lrCiux3XvrNB6K86dvc';
+const GEMINI_API_KEY = 'your_api';
 const GEMINI_API_URL = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent';
 
 export interface GeminiResponse {
